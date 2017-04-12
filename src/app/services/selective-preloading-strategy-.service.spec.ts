9f723f273e90dce5ef16f9870beb7790ba3724c5
simon <simon_an18@hotmail.com>
2017-04-12 23:23:43 +0200

    minor changes

--- src/app/services/selective-preloading-strategy-.service.spec.ts
+++ src/app/services/selective-preloading-strategy-.service.spec.ts
@@ -1,15 +1,15 @@
-import { TestBed, inject } from '@angular/core/testing';
+import { inject, TestBed } from '@angular/core/testing';
 
-import { SelectivePreloadingStrategyService } from './selective-preloading-strategy-.service';
+import { SelectivePreloadingStrategy } from './selective-preloading-strategy-.service';
 
 describe('SelectivePreloadingStrategyService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [SelectivePreloadingStrategyService]
+      providers: [SelectivePreloadingStrategy],
     });
   });
 
-  it('should ...', inject([SelectivePreloadingStrategyService], (service: SelectivePreloadingStrategyService) => {
+  it('should ...', inject([SelectivePreloadingStrategy], (service: SelectivePreloadingStrategy) => {
     expect(service).toBeTruthy();
   }));
 });
